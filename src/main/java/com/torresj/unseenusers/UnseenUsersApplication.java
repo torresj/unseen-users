d710cba9edc837ee1cb15a226dbb549bd72f004b
Jaime Torres <Jaime.Torres@liberty-it.co.uk>
2023-01-30 12:36:22 +0100

    first structure

---
+++ src/main/java/com/torresj/unseenusers/UnseenUsersApplication.java
@@ -0,0 +1,13 @@
+package com.torresj.unseenusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnseenUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnseenUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/torresj/unseenusers/UnseenUsersApplicationTests.java
@@ -0,0 +1,17 @@
+package com.torresj.unseenusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+class UnseenUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
